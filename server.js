e1d20f6e3e5acfb3f0422655482fbf987617bd80
Douglas Parker <dgp1130422@gmail.com>
2016-12-16 22:58:58 -0800

    Added server-side Uniform validation.

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 var express = require("express");
 var app = express();
 
+// Load Uniform
+var validator = require("uniform-validation");
+
 // Serve the www/ directory to clients
 app.use(express.static("www"));
 
-// On POST to /submit, validate the form
-app.post("/submit", function (req, res) {
-    res.end("I guess it's valid?");
+// On POST to /submit, validate with Uniform
+app.post("/submit", validator("www/car.ufm"), function (req, res) {
+    res.end("Valid :)");
+}, function (err, req, res, next) {
+    res.end("Invalid :(");
+    console.error(err);
 });
 
 // Listen on port 8000
